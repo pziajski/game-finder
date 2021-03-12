9d17cddcb93af930d3df18dd36e083821b1a0e17
Patrick Ziajski <patrick.ziajski@gmail.com>
2021-03-12 13:22:24 -0500

    reset app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import GamesPage from './pages/GamesPage/GamesPage';
-import games from './assets/data/temp-game-list.json';
 
 export default class App extends Component {
 	render() {
     	return (
 			<div>
-				<GamesPage games={games} />
 			</div>
 	    )
 	}
