9f952272606af59440fadf3609d8df3c0b722704
Patrick Ziajski <patrick.ziajski@gmail.com>
2021-03-13 17:17:53 -0500

    imported correct json file

--- src/pages/HomePage/Homepage.js
+++ src/pages/HomePage/Homepage.js
@@ -1,25 +1,25 @@
-import options from '../../Data/Options';
 import './Homepage.scss';
-import arrow from '../../images/down-arrow.svg'
+import arrow from '../../images/down-arrow.svg';
 
-function Homepage(props) {
+import genres from '../../assets/data/genres.json';
+
+function Homepage({ submit }) {
     return(
         <div className="homepage">
             <h1 className="homepage__title">FindIt</h1>
             <h2 className='homepage__info'>Bored and Broke no Problem </h2>
-            <form className='homepage__form' onSubmit={props.submit}>
-            <label className='homepage__form--label'>Pick a Genere 
+            <form className='homepage__form' onSubmit={submit}>
+            <label className='homepage__form--label'>Pick a Genre 
                 <select className="homepage__form--picker" name='select' defaultValue='none'>
                     <option value='none'>none</option>
-                    {options.map(genre => 
-                        <option key={genre} value={genre}> {genre}</option>
+                    {genres.map(genre => 
+                        <option key={genre.id} value={genre.name}> {genre.name}</option>
                     )}
                 </select>
             </label>
             <button className='homepage__form--btn'>Submit</button>
             </form>
-            <img className="homepage__arrow" src={arrow}></img>
-
+            <img className="homepage__arrow" src={arrow} alt='arrow' />
         </div>
     )
 
